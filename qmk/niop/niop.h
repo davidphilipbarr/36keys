790dc908660eadb2005665d6b34abdad754f629b
David Barr <dpbarr@gmail.com>
2020-07-17 10:07:37 +0100

    half baked

---
+++ qmk/niop/niop.h
@@ -0,0 +1,12 @@
+#ifndef KB_H
+#define KB_H
+
+#include "quantum.h"
+
+#define KEYMAP( \
+	K01, K02, K03, K04, K05, K06, K07, K08, K09, K10 \
+) { \
+	{ K01, K02, K03, K04, K05, K06, K07, K08, K09, K10 }  \
+}
+
+#endif
