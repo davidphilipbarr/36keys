1e3f6935bbcce624a02e82799d1f8be810ae8de1
David Barr <dpbarr@gmail.com>
2020-11-19 16:07:36 +0000

    Add files via upload

--- qmk/macro2/keymaps/default/keymap.c
+++ qmk/macro2/keymaps/default/keymap.c
@@ -1,13 +1,12 @@
 #include "macro2.h"
-#define KC_SSHOT SGUI(KC_4)
-#define KC_MISSION LCTL(KC_UP)
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	KEYMAP(
+	LAYOUT(
 	KC_BRMD, 	KC__MUTE, KC_MEDIA_PLAY_PAUSE, KC_F11, 
 
-	    KC_LGUI, KC_MNXT, RGB_MODE_FORWARD, RGB_TOG ),
+	  KC_LGUI, KC_MNXT, KC_A, KC_B),
 
 	
 
